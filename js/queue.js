bb18473314ae314d8acc3a9a0be6bafec7299a6d
Dave Everitt <deveritt@innotts.co.uk>
2019-03-22 22:45:09 +0000

    all queue functionality in interface

--- js/queue.js
+++ js/queue.js
@@ -26,28 +26,49 @@ const createQueue = () => {
 const q = createQueue();
 // console.log(q.isEmpty());
 
-q.enqueue('Stop hacking');
-q.enqueue('Make dinner');
-q.enqueue('Eat dinner');
-q.enqueue('Wash up');
-// console.log(q.listQueue());
-
-// console.log(q.peek());
-// q.dequeue();
-// console.log(q.peek());
-
 /* =================================
- Optional code to show on the screen
+   Optional: queue items from form
    ================================= */
 
-const showList = document.getElementById("showlist");
+const showQueue = document.getElementById("showQueue");
+const setQueue = document.getElementById("setQueue");
+const queueEls = setQueue.elements;
+const getItems = () => document.querySelectorAll('[data-choice^="item"]');
 
 const makeList = (el, index) => {
-  showList.innerHTML += `<input type="checkbox" id="item${index}" name="choices" value="item${index}"><label for="item${index}">${el}</label><br>`;
+  showQueue.innerHTML += `<input type="checkbox" id="item${index}" name="choices" value="item${index}"><label for="item${index}">${index}: ${el}</label> <span data-choice="item${index}"> x </span><br>`;
 }
-q.listQueue().map(makeList);
 
-// q.listQueue().map((el, index) => {
-//   showList.innerHTML += `<input type="checkbox" id="item${index}" name="choices" value="item${index}"><label for="item${index}">${el}</label><br>`;
-// });
+const popItem = (e) => {
+  q.dequeue();
+  redrawList();
+}
+
+const buttonClick = () => showQueue[showQueue.length-1].addEventListener("click", popItem);
+
+const redrawList = () => {
+  showQueue.innerHTML = '';
+  q.listQueue().map(makeList);
+  if (q.isEmpty() !== true) {
+    showQueue.innerHTML += `<button>dequeue</button>`;
+    buttonClick();
+  }
+  [...getItems()].map(showItems);
+}
+
+const addItem = (e) => {
+  q.enqueue(queueEls[0].value);
+  queueEls[0].value = '';
+  redrawList();
+  e.preventDefault(e);
+}
+setQueue.addEventListener('submit', addItem);
+
+// toggle highlight selected item
+const showItems = (e) => {
+  e.addEventListener("click", () => {
+    e.classList.toggle("selected");
+    e.previousElementSibling.classList.toggle("highlight");
+  });
+}
 
